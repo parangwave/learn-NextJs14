c2d58ba40fa7dbe2e8ed6284d205ab560d435895
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-23 00:21:51 +0900

    Routing - Hydration
    SSR을 통해 만들어 진 HTML을 클라이언트 측 JS를 사용하여 interactive react component로 변환

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,11 +3,17 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   // path name; url a user stay
   const path = usePathname(); // only works in client component -> "use client"
-  console.log(path);
+  // console.log(path);
+
+  // hydration = SSR을 통해 만들어진 html이 인터랙티브한 react 컴포넌트로 변환되는 과정
+  // /about-us -> <button>0</button> -> user get html -> <button onClick={}></button>
+  // -> init(html) into interactive react component w/ event-listener
+  const [count, setCount] = useState(0);
 
   return (
     <nav>
@@ -19,6 +25,9 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About us</Link>{" "}
           {path === "/about-us" ? "📜" : ""}
+        </li>
+        <li>
+          <button onClick={() => setCount((c) => c + 12)}>{count}</button>
         </li>
       </ul>
     </nav>
