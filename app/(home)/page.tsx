cc5a02df0e04a3122ec3208c4ea07e6004bc4b35
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-26 02:59:00 +0900

    Data Fetching - Parallel Requests
    Promise.all는 병렬(동시에) fetch 처리하며 결과값 array를 return함

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,18 @@
 // not in client
+import Link from "next/link";
 export const metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   // loading state moved, disappeared X
-  await new Promise((resolve) => setTimeout(resolve, 10000)); // trick to slow down
+  // await new Promise((resolve) => setTimeout(resolve, 1000)); // trick to slow down
 
   // 첫번째 요청으로 메모리에서 받은 fetch된 데이터만 받음
   // fetch된 URL을 캐싱해줌 b/c NextJS === fwk
-  return fetch(URL).then((response) => response.json());
+  return fetch(API_URL).then((response) => response.json());
   // const resp = await fetch(URL);
   // const json = await resp.json();
   // return json;
@@ -20,7 +21,15 @@ async function getMovies() {
 // 어떤 일이 발생하길 기다리려고 await를 사용할 때, 부모 함수에 async가 무조건 있어야함
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
 
 // streaming -> nextJs는 우리의 페이지를 작은 html부분으로 나눠서 준비된 html부분을 브라우저에게 줄 수 있음
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,41 @@
 // dynamic routes = [folder name]
 
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const resp = await fetch(`${API_URL}/${id}`);
+  return resp.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   // searchParams 검색 페이지 구축할 때 유용
   params: { id }, // var in url
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  console.log("===========");
+  console.log("start fetching");
+
+  // Promise.all: 동시에 movie, videos를 await = 병렬 처리(Parallel Requests, 순차적X)
+  // 5 sec + API time
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+  console.log("end fetching");
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+    </div>
+  );
 }
