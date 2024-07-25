a5c2c97984de7417dd817453e0691f201de72d9d
Oh Jisu <parangwavetraveller@gmail.com>
2024-07-26 02:33:21 +0900

    Data Fetching - Loading Components
    streming을 사용하면서 nextJs는 우리의 페이지를 작은 html부분으로 나눠서 준비된 html부분을 브라우저에게 줄 수 있음
    그리고 브라우저에게 BE에서 통신이 아직 마무리 되지 않아 기다려야 한다고 알려줌.
    await가 끝나면 브라우저에게 마지막 html의 부분을 줌.
    그럼 프레임워크가 loading component를 await된 component로 바꿔주는 것임.

---
+++ app/(home)/loading.tsx
@@ -0,0 +1,5 @@
+// 파일 이름은 무조건 loading!!!
+// page 파일 옆에 있어야 함!!!
+export default function Loading() {
+  return <h2>Loading...</h2>;
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,7 +7,7 @@ const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   // loading state moved, disappeared X
-  await new Promise((resolve) => setTimeout(resolve, 5000)); // trick to slow down
+  await new Promise((resolve) => setTimeout(resolve, 10000)); // trick to slow down
 
   // 첫번째 요청으로 메모리에서 받은 fetch된 데이터만 받음
   // fetch된 URL을 캐싱해줌 b/c NextJS === fwk
@@ -22,3 +22,17 @@ export default async function HomePage() {
   const movies = await getMovies();
   return <div>{JSON.stringify(movies)}</div>;
 }
+
+// streaming -> nextJs는 우리의 페이지를 작은 html부분으로 나눠서 준비된 html부분을 브라우저에게 줄 수 있음
+// 브라우저에게 백엔드에서 통신이 아직 마무리 되지 않아 기다려야 한다고 알려줌
+// fetch가 완료될 때 까지 브라우저에게 기다려라고 함 -> loading content
+// fetch가 끝나고 통신 완료, await가 끝남 -> 브라우저에게 마지막 html의 부분을 줌
+// 프레임워크가 loading component를 await된 component로 바꿔주는 것
+
+// Be에서 data를 fetching하며 useState와 같은 hooks 사용 필요 X
+// 즉각적 loading state & loading UI content
+// 즉, 통신이 마무리 될 때 프레임워크에 의해 실제 결과값으로 자동 교체
+
+// <Loading />
+// const html = await HomePage()
+// isLoaing ? <Loading />
